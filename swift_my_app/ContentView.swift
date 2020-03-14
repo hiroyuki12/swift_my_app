61a306f44a1fa43ca90c8f074190de93b9eea9d2
hiroyuki12 <hiroyuki12@gmail.com>
2020-03-15 00:15:59 +0900

    add print Flutter Issues. URLSession.

--- swift_my_app/ContentView.swift
+++ swift_my_app/ContentView.swift
@@ -10,7 +10,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Text("Hello, World!")
+            Button(action: {
+//                print("Button Tapped")
+//                let url: URL = URL(string: "http://qiita.com/api/v2/items")!  // http error
+                let url: URL = URL(string:  "https://api.github.com/repositories/31792824/issues")!
+                let task: URLSessionTask = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+                    do{
+                        let json = try JSONSerialization.jsonObject(with: data!)
+                        print("json!!!!!!!!")
+                        print(json)
+                    }
+                    catch{
+//                    print("data: \(String(describing: data))")
+//                    print("response: \(String(describing: response))")
+//                    print("error: \(String(describing: error))")
+                    }
+                })
+                task.resume() //実行する
+            }){
+                Text("Button")
+            }
+        }
     }
 }
 
