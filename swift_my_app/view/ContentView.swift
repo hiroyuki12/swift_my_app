0b58a9492ec901fa69e264074970c705ac23c9ff
hiroyuki12 <hiroyuki12@gmail.com>
2020-03-16 22:36:53 +0900

    Add Flutter Issues.

--- swift_my_app/view/ContentView.swift
+++ swift_my_app/view/ContentView.swift
@@ -13,36 +13,39 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack{
+                NavigationLink(destination: FlutterIssues()) {
+                    Text("NavigationLink Flutter Issues")
+                }
                 NavigationLink(destination: MyImage()) {
                     Text("NavigationLink Image")
                 }
                 NavigationLink(destination: HelloWorld()) {
                     Text("NavigationLink HelloWorld")
                 }
-                NavigationLink(destination: MyActionSheet()) {
-                    Text("NavigationLink ActionSheet")
-                }
-                NavigationLink(destination: MyAlert()) {
-                    Text("NavigationLink Alert")
-                }
-                NavigationLink(destination: MyTabView()) {
-                    Text("NavigationLink TabView")
-                }
-                NavigationLink(destination: MyList()) {
-                    Text("NavigationLink List")
-                }
-                NavigationLink(destination: MyPicker()) {
-                    Text("NavigationLink Picker")
-                }
-                NavigationLink(destination: MyButton()) {
-                    Text("NavigationLink Button")
-                }
-                NavigationLink(destination: SFSymbols()) {
-                    Text("NavigationLink SFSymbols")
-                }
-                NavigationLink(destination: MyWebView()) {
-                    Text("NavigationLink WebView")
-                }
+//                NavigationLink(destination: MyActionSheet()) {
+//                    Text("NavigationLink ActionSheet")
+//                }
+//                NavigationLink(destination: MyAlert()) {
+//                    Text("NavigationLink Alert")
+//                }
+//                NavigationLink(destination: MyTabView()) {
+//                    Text("NavigationLink TabView")
+//                }
+//                NavigationLink(destination: MyList()) {
+//                    Text("NavigationLink List")
+//                }
+//                NavigationLink(destination: MyPicker()) {
+//                    Text("NavigationLink Picker")
+//                }
+//                NavigationLink(destination: MyButton()) {
+//                    Text("NavigationLink Button")
+//                }
+//                NavigationLink(destination: SFSymbols()) {
+//                    Text("NavigationLink SFSymbols")
+//                }
+//                NavigationLink(destination: MyWebView()) {
+//                    Text("NavigationLink WebView")
+//                }
                 
             }
         }
---
+++ swift_my_app/view/FlutterIssues.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  swift_my_app
+//
+//  Created by hiroyuki on 2020/03/14.
+//  Copyright © 2020 hiroyuki. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct FlutterIssues: View {
+    var body: some View {
+        List{
+            Button(action: {
+                let url: URL = URL(string:  "https://api.github.com/repositories/31792824/issues")!
+                let task: URLSessionTask = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+                    do{
+                        let json = try JSONSerialization.jsonObject(with: data!)
+                        let top = json as! NSArray // トップレベルが配列
+                        for roop in top {
+                            let next = roop as! NSDictionary
+                            print(next["title"] as! String) // タイトル表示
+                        }
+                    }
+                    catch{
+                    }
+                })
+                task.resume() //実行する
+            }){
+                Text("取得")
+            }
+        }
+    }
+}
+
+struct FlutterIssues_Previews: PreviewProvider {
+    static var previews: some View {
+        FlutterIssues()
+    }
+}
+
